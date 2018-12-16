8ab8320919b098a42df7676f4a570ca6a31ebaf2
Himank Goel <himankgoel03@gmail.com>
2018-12-16 17:41:06 +0530

    Completed

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from random import shuffle
 
+
 def read_file():
-    wordlist=[]
+    wordlist = []
     for line in open("input.txt"):
         wordlist.append(line.split("\n")[0])
-    
+
     return wordlist
+
 
 wordlist = read_file()
 
 jumbled_list = wordlist[:]
-
 shuffle(jumbled_list)
-print jumbled_list
+
+output_dict = {}
+
+for word in jumbled_list:
+    definition = raw_input(word + " ")
+    type(definition)
+    output_dict[word] = definition
+    # print output_dict
+
+f = open("output.txt", "w+")
+
+for word in wordlist:
+    f.write("%s - %s\n" % (word, output_dict[word]))
+    # print output_dict[word]
+
+# print jumbled_list
